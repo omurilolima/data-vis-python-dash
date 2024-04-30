3e9b6fb9df6376d3b8045c95225ae834618345e4
omurilolima <muriloserafimlima@gmail.com>
2024-04-30 18:29:35 +0100

    update style in style_arguments.py

--- 03_02/style_arguments.py
+++ 03_02/style_arguments.py
@@ -5,7 +5,8 @@ import plotly.express as px
 import pandas as pd
 
 # Read in the data
-data = pd.read_csv("precious_metals_prices_2018_2021.csv", usecols=["DateTime", "Gold"])
+data = pd.read_csv("precious_metals_prices_2018_2021.csv",
+                   usecols=["DateTime", "Gold"])
 
 # Create a plotly plot for use by dcc.Graph(). Notice list for y= to make color_discrete_map work
 fig = px.line(
@@ -35,15 +36,20 @@ app.layout = html.Div(
     children=[
         html.Div(
             id="header-area",
+            style={'backgroundColor': 'black'},
             children=[
                 html.H1(
                     id="header-title",
+                    style={'color': 'white',
+                           'fontFamily': "Verdana, sans-serif"},
                     children="Precious Metal Prices",
-
                 ),
                 html.P(
                     id="header-description",
-                    children=("The cost of precious metals", html.Br(), "between 2018 and 2021"),
+                    style={'color': 'white',
+                           'fontFamily': "Verdana, sans-serif"},
+                    children=("The cost of precious metals",
+                              html.Br(), "between 2018 and 2021"),
                 ),
             ],
         ),
